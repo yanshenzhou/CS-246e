29f091d3e9608982ab2b4be32223f60e53bb3301
bmlushma <bmlushma@uwaterloo.ca>
2021-11-09 18:07:33 -0500

    Observer Pattern

---
+++ lectures/observer/bettor.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "observer.h"
+#include "horserace.h"
+#include "bettor.h"
+
+using namespace std;
+
+Bettor::Bettor(HorseRace *hr, std::string name, std::string horse):
+  subject{hr}, name{name}, myHorse{horse} {
+  subject->attach(this);
+}
+
+Bettor::~Bettor() {
+  subject->detach(this);
+}
+
+void Bettor::notify() {
+  string winner = subject->getState();
+  cout << name << (winner == myHorse ? " wins!" : " loses.") << endl;
+}
+
